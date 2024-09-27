34e4ae858de26f9defe08457f3f51b292127c927
unknown <nowywy@gmail.com>
2024-09-27 04:31:59 +0200

    last one before bed, just sync

--- Assets/PlayerSessionComponent.cs
+++ Assets/PlayerSessionComponent.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.Netcode;
-using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -29,6 +28,11 @@ public class PlayerSessionComponent : NetworkBehaviour
     }
 
     private void handleClientStopped(bool wasHost){
+        StartCoroutine(desperateLoadSceneDelay());
+    }
+
+    private IEnumerator desperateLoadSceneDelay(){
+        yield return new WaitForSeconds(2.0f);
         SceneManager.LoadScene("PlayScene");
     }
 
@@ -36,56 +40,6 @@ public class PlayerSessionComponent : NetworkBehaviour
         NetworkManager.OnClientStopped-=handleClientStopped;
         base.OnDestroy();
     }
-
-    /*private IEnumerator ClientShutdownCoroutine(bool exitGame)
-    {
-        Debug.Log("Shutdown called for client " + NetworkManager.LocalClientId);
-        
-        // Call the async shutdown method and wait for it to complete
-        //var shutdownTask = ShutdownAsync();
-        print("waiting for async shutdown to complete");
-        NetworkManager.Singleton.Shutdown();
-        NetworkManager.
-        //yield return new WaitUntil(() => shutdownTask.IsCompleted);
-        print("async shutdown completed");
-        if (exitGame)
-        {
-            Application.Quit();
-        }
-        else
-        {
-            SceneManager.LoadSceneAsync("PlayScene");
-            // MainMenuManager.Instance.EnableMainMenu();
-        }
-    }
-
-    private async Task ShutdownAsync()
-    {
-        NetworkManager.Singleton.Shutdown();
-        NetworkManager.Singleton.GetComponent<MyNetworkDiscovery>().StopDiscovery();
-
-        // Wait until the shutdown process is complete
-        while (NetworkManager.Singleton.ShutdownInProgress)
-        {
-            await Task.Yield(); // Yield control back to the main thread
-        }
-    }*/
-    
-    /*private IEnumerator HostShutdown(bool exitGame)
-    {
-        // Tell all clients to shutdown
-        print("HostShutdown called, sending shutdown message to clients");
-        //ShutdownClientRpc(false);
-        //foreach (var client in NetworkManager.ConnectedClientsList)
-        //    DisconnectClientServerRpc(client.ClientId);
-
-        // Wait some time for the message to get to clients
-        yield return new WaitForSeconds(0.5f);
-        print("Shutting down host itself after 0.5s wait");
-        // Shutdown server/host
-        Shutdown(exitGame);
-    }*/
-
     [ServerRpc]
     private void DisconnectClientServerRpc(ulong playerID){
         print($"disconnect request made to server for {playerID}");
