0fbb5a8963d5acacd8e91cbd1fca3a194cbdcb56
unknown <nowywy@gmail.com>
2024-09-27 02:17:54 +0200

    attempt at fixing session flow

--- Assets/PlayerSessionComponent.cs
+++ Assets/PlayerSessionComponent.cs
@@ -9,28 +9,38 @@ public class PlayerSessionComponent : NetworkBehaviour
     public void beginShutDown(bool exitGame=false)
     {
         print("beginshutdown called for "+NetworkManager.LocalClientId);
+        
         //caller.
         //print("beginshutdown called for "+caller.playerName.Value);
         if (IsServer)
-            StartCoroutine(HostShutdown(exitGame));
-        else
             Shutdown(exitGame);
+            //StartCoroutine(HostShutdown(exitGame));
+        else if(IsOwner)
+            DisconnectClientServerRpc(NetworkManager.LocalClientId);
     }
     
-    private IEnumerator HostShutdown(bool exitGame)
+    /*private IEnumerator HostShutdown(bool exitGame)
     {
         // Tell all clients to shutdown
         print("HostShutdown called, sending shutdown message to clients");
-        ShutdownClientRpc(false);
+        //ShutdownClientRpc(false);
+        //foreach (var client in NetworkManager.ConnectedClientsList)
+        //    DisconnectClientServerRpc(client.ClientId);
 
         // Wait some time for the message to get to clients
         yield return new WaitForSeconds(0.5f);
         print("Shutting down host itself after 0.5s wait");
         // Shutdown server/host
         Shutdown(exitGame);
+    }*/
+
+    [ServerRpc]
+    private void DisconnectClientServerRpc(ulong playerID){
+        print($"disconnect request made to server for {playerID}");
+        NetworkManager.DisconnectClient(playerID);
     }
     
-    [ClientRpc]
+    /*[ClientRpc]
     private void ShutdownClientRpc(bool exitGame)
     {
         print("ShutdownClientRpc called "+NetworkManager.LocalClientId+" IsOwner?: "+IsOwner);
@@ -38,7 +48,8 @@ public class PlayerSessionComponent : NetworkBehaviour
         //if(IsHost) return;
 
         Shutdown(exitGame);
-    }
+    }*/
+
     private void Shutdown(bool exitGame)
     {
         print("Shutdown called "+NetworkManager.LocalClientId);
